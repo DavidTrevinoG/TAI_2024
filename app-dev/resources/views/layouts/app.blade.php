af4c1f91de8d116000e2e840523698916f47ad93
David Treviño <2130059@upv.edu.mx>
2024-05-14 07:58:31 -0600

    Added Styles

--- app-dev/resources/views/layouts/app.blade.php
+++ app-dev/resources/views/layouts/app.blade.php
@@ -1,12 +1,17 @@
 <html>
     <title>Portal - @yield('title')</title>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+    </head>
     <body>
         <nav>
             <a href="/">Página principal</a>
             <a href="/Alumnos">Alumnos</a>
             <a href="/Maestros">Maestros</a>
         </nav>
-        <h1>@yield('title')</h1>
+        <h1 class="text-3xl font-bold underline">@yield('title')</h1>
         <br>
         <h3>@yield('content')</h3>
     </body>
